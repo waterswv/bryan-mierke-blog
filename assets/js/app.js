f0bf694d72fb375c16e5e77ccddc47eb39732177
Bryan <bryanmierke-macbookair@Bryans-MacBook-Air.local>
2018-02-02 13:53:31 -0800

    added id='sideposts' to 1st section tag to eliminate duplicate posts

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,26 +5,26 @@ $(document).ready( function(){
   // This is TEST Data for the Template.
   let posts =
   [{
-    sidePostDate: 'Num 1',
+    sidePostDate: 'Jan 30',
     sidePostURL: '#',
     sidePostTitle: 'Understanding Primitives',
     sidePostContent: "A primitive (primitive value, primitive data type) is data that is not an object and has no methods. In JavaScript, there are 6 primitive data types..."
   },
   {
-    sidePostDate: 'Num 2',
-    sidePostURL: '#',
-    sidePostTitle: 'Understanding Primitives',
-    sidePostContent: "A primitive (primitive value, primitive data type) is data that is not an object and has no methods. In JavaScript, there are 6 primitive data types..."
+    sidePostDate: 'Jan 2',
+    sidePostURL: 'http://exploringjs.com/es6/ch_destructuring.html',
+    sidePostTitle: 'Destructuring',
+    sidePostContent: "Destructuring is a convenient way of extracting multiple values from data stored in (possibly nested) objects and Arrays. It can be used..."
   },
   {
-    sidePostDate: 'Num 3',
-    sidePostURL: '#',
-    sidePostTitle: 'Understanding Primitives',
-    sidePostContent: "A primitive (primitive value, primitive data type) is data that is not an object and has no methods. In JavaScript, there are 6 primitive data types..."
+    sidePostDate: 'Dec 3',
+    sidePostURL: 'https://medium.com/@deniswells59/singly-linked-lists-with-javascript-81859a5a5095"',
+    sidePostTitle: 'Singly Linked List',
+    sidePostContent: "In a linked list, a node has a pointer to the next node. So, what would that look like in JS? We explore in this post..."
   }];
 
-  // Grab each Post object and iterate to publish to the page. 
-  posts.forEach( post => {$('#sidebar > section > .divided').append(sidePostTemplate(post));});
+  // Grab each Post object and iterate to publish to the page.
+  posts.forEach( post => {$('#sidebar > #sideposts > .divided').append(sidePostTemplate(post));});
 
 
   // Function to generate template HTML for each smaller sidePost.
@@ -37,7 +37,7 @@ $(document).ready( function(){
         <article class="box excerpt">
           <header>
             <span class="date">${post.sidePostDate}</span>
-            <h3><a href="${post.sidePostURL}">${post.sidePostTitle}</a></h3>
+            <h3><a href="${post.sidePostURL}" target='_blank'>${post.sidePostTitle}</a></h3>
           </header>
           <p>${post.sidePostContent}
           </p>
